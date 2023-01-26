f0e60f605e0f7b414197276e92799f7e46a72611
MattHelton <mjoelhelton@me.com>
2023-01-26 09:51:56 -0500

    adds initial prompts

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const inquirer = require("inquirer")
+
+var possibleCharacters = ``
+var lowerCase = `abcdefghijklmnopqrstuvwxyz`
+var upperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+var specialChars = `!@#$%^&*()_+=-?/><{}[]`
+
+inquirer.prompt([
+    {
+        message: 'How long should the password be?',
+        name: `passwordLength`,
+        type: `input`
+    },
+    {
+        message: `Would you like numbers in your password?`,
+        name: `hasNumbers`,
+        type: `input`
+    },
+    {
+        message: `Would you like capital letters in your password?`,
+        name: `hasCaps`,
+        type: `input`
+    },
+    {
+        message: `Would you like special characters in your password?`,
+        name: `hasSpecials`,
+        type: `input`
+    }
+
+]).then((answers) => {
+    var passwordLength = answers.passwordLength
+})
